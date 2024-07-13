da40d93bb532663534f217ff39f8077481423e06
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2024-07-14 01:01:05 +0530

    adding user registration backend

---
+++ backend/controller/userController.js
@@ -0,0 +1,38 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import { User } from "../models/userSchema.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+
+export const patientRegister = catchAsyncErrors(async (req, res, next) => {
+  const { firstName, lastName, email, phone, nic, dob, gender, password } =
+    req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !nic ||
+    !dob ||
+    !gender ||
+    !password
+  ) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(new ErrorHandler("User already Registered!", 400));
+  }
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    password,
+    role: "Patient",
+  });
+  generateToken(user, "User Registered!", 200, res);
+});
