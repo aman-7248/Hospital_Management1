0dba84ffcd127c5850b68c8e294ca5a4c2e57932
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2024-07-14 18:13:10 +0530

    added the feature to add new doctor

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -3,6 +3,7 @@ import {
   addNewAdmin,
     login,
   patientRegister,
+  addNewDoctor,
   getAllDoctors,
   getUserDetails,
   logoutPatient,
@@ -16,6 +17,7 @@ const router = express.Router();
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
 router.post("/admin/addnew", isAdminAuthenticated,addNewAdmin);
+router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
 router.get("/doctors", getAllDoctors);
 router.get("/patient/me", isPatientAuthenticated, getUserDetails);
 router.get("/admin/me", isAdminAuthenticated, getUserDetails);
