8ad78514f45600abc3a0f844ed3a3454d8d124b1
Abhay <abhaysingh8887810379@gmail.com>
2024-07-25 19:42:13 +0530

    removed errors

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -1,7 +1,8 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
-import ErrorHandler from "../middlewares/error.js";
+import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { Appointment } from "../models/appointmentSchema.js";
 import { User } from "../models/userSchema.js";
+
 
 export const postAppointment = catchAsyncErrors(async (req, res, next) => {
   const {
