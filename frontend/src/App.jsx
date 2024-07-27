bd48634ddb315ca263a41300dd9ec15dea731d6c
Abhay <abhaysingh8887810379@gmail.com>
2024-07-27 15:29:49 +0530

    Adding Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,40 @@ import Home from "./Pages/Home";
 import Appointment from "./Pages/Appointment";
 import AboutUs from "./Pages/AboutUs";
 import Register from "./Pages/Register";
-import Navbar from "./components/Navbar";
 import Login from "./Pages/Login";
+import Navbar from "./components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Context } from "./main";
 
 const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+
   return (
     <>
       <Router>
-      <Navbar />
-      <Routes>
+        <Navbar />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/appointment" element={<Appointment />} />
           <Route path="/about" element={<AboutUs />} />
