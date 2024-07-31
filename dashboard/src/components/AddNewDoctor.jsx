6c0ba75bc7eea462a4cd4cc6cbbe27317e86f77d
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 13:11:57 +0530

    added paramters in addNewDoctor function

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const AddNewDoctor = () => {
-  return (
-    <div>AddNewDoctor</div>
-  )
-}
-
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [nic, setNic] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+  const [doctorDepartment, setDoctorDepartment] = useState("");
+  const [docAvatar, setDocAvatar] = useState("");
+  const [docAvatarPreview, setDocAvatarPreview] = useState("");
+  const navigateTo = useNavigate();
+  return <div>AddNewDoctor</div>;
+};
 export default AddNewDoctor;
