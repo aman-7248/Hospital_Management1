7c39064e5f33d859a4412f3387930be92c22956a
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 19:37:33 +0530

    import all required objects

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import axios from "axios";
 const AddNewDoctor = () => {
   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
   const [firstName, setFirstName] = useState("");
