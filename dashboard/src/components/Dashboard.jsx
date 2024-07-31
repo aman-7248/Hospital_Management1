e8a3e59f96a19351bc41919a863ae095648f0501
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 19:49:00 +0530

    dashbord function created

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../main";
+import axios from "axios";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/appointment/getall",
+          { withCredentials: true }
+        );
+        setAppointments(data.appointments);
+      } catch (error) {
+        setAppointments([]);
+      }
+    };
+    fetchAppointments();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+};
 
 export default Dashboard;
