f4ddc7ed44538aa982734e56a09fb5f3a31e3ce2
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 19:21:31 +0530

    page design completed

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -93,7 +93,38 @@ const AddNewDoctor = () => {
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
-
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Mobile Number"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="NIC"
+                value={nic}
+                onChange={(e) => setNic(e.target.value)}
+              />
+              <input
+                type={"date"}
+                placeholder="Date of Birth"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+              />
+              <select
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
               <input
                 type="password"
                 placeholder="Password"
