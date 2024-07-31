75d86fa000671764e814115738ec3bfb5d13fae1
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 13:25:10 +0530

    handling new doctor

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -14,6 +14,43 @@ const AddNewDoctor = () => {
   const [docAvatar, setDocAvatar] = useState("");
   const [docAvatarPreview, setDocAvatarPreview] = useState("");
   const navigateTo = useNavigate();
+
+  const handleAddNewDoctor = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("firstName", firstName);
+      formData.append("lastName", lastName);
+      formData.append("email", email);
+      formData.append("phone", phone);
+      formData.append("password", password);
+      formData.append("nic", nic);
+      formData.append("dob", dob);
+      formData.append("gender", gender);
+      formData.append("doctorDepartment", doctorDepartment);
+      formData.append("docAvatar", docAvatar);
+      const response = await axios
+        .post("http://localhost:4000/api/v1/user/doctor/addnew", formData, {
+          withCredentials: true,
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPhone("");
+          setNic("");
+          setDob("");
+          setGender("");
+          setPassword("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
   return <div>AddNewDoctor</div>;
 };
 export default AddNewDoctor;
