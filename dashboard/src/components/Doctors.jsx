07757a8675c7271b3a7e881882dad57b646f9cae
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 19:33:50 +0530

    functionalities added in doctor

--- dashboard/src/components/Doctors.jsx
+++ dashboard/src/components/Doctors.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Doctors = () => {
-  return (
-    <div>Doctors</div>
-  )
-}
+  const [doctors, setDoctors] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/user/doctors",
+          { withCredentials: true }
+        );
+        setDoctors(data.doctors);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
+    fetchDoctors();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+};
 
 export default Doctors;
