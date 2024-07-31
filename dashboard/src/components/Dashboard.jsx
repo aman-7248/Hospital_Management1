b10f6b0b92852b426bbaa4939867601f6eab4191
Aman kumar singh <amankrsingh2079@gmail.com>
2024-07-31 19:53:19 +0530

    some designing done

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -23,6 +23,53 @@ const Dashboard = () => {
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />;
   }
+  return (
+    <>
+      <section className="dashboard page">
+        <div className="banner">
+          <div className="firstBox">
+            <img src="/doc.png" alt="docImg" />
+            <div className="content">
+              <div>
+                <p>Hello ,</p>
+                <h5>{admin && `${admin.firstName} ${admin.lastName}`} </h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facilis, nam molestias. Eaque molestiae ipsam commodi neque.
+                Assumenda repellendus necessitatibus itaque.
+              </p>
+            </div>
+          </div>
+          <div className="secondBox">
+            <p>Total Appointments</p>
+            <h3>1500</h3>
+          </div>
+          <div className="thirdBox">
+            <p>Registered Doctors</p>
+            <h3>10</h3>
+          </div>
+        </div>
+        <div className="banner">
+          <h5>Appointments</h5>
+          <table>
+            <thead>
+              <tr>
+                <th>Patient</th>
+                <th>Date</th>
+                <th>Doctor</th>
+                <th>Department</th>
+                <th>Status</th>
+                <th>Visited</th>
+              </tr>
+            </thead>
+          </table>
+
+          {}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Dashboard;
